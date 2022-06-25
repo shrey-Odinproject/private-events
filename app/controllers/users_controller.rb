505fd4e38040b5c857e85695116d03a762976777
shrey-Odinproject <sonishreyf1@gmail.com>
2022-06-25 15:20:16 +0530

    Introduce logic to display atendee link/button on event's show page depending on who created the event
    (a creator viewing their own event's details cannot be that
    same event's atendee too)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user! 
+  # before_action :authenticate_user!  dont need this as u wont be able to acces page with all of user's created events if u aint loggrd in 
   # ,except:[:index]
   def show
     # @user = User.find(params[:id])
