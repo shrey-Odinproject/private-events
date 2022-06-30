d240ef1e912b2a951f993f3f931774c95ca1111a
shrey-Odinproject <sonishreyf1@gmail.com>
2022-06-30 14:27:49 +0530

    Simplify attending and unattending event
    Earlier methods worked but were not clean, this new way is clean and
    easy to understand (no more manually creating forms with hidden fields
    to pass attributes to desired controllers, no more querying in the view
    itself to get the exact invitation path to delete where params[:id]
    gives us the excat invitation we want to delete). THIS IS AN IMP CHANGE
    
    I picked this pattern from another user and will use it as template for
    similar problems

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,39 +1,25 @@
 class InvitationsController < ApplicationController
-  # removed the new method because @invitation was never used in any view, we needed instance of invitation to create a form in 
-  #other controller's view (event) so we directly created an instance there in view itself rather than use @invitation created in 
-  # controller#new 
-
-  # def new
-  #   @invitation = Invitation.new
-  # end
 
   def create
-    @invitation = current_user.invitations.build(invitations_params) # major step
+    current_event = Event.find(params[:event_id]) # we maually pass event_id in params (check event#show view)
+    @invitation = current_user.invitations.build(attended_event_id: current_event.id)
 
     if @invitation.save
-      flash[:success] = "U are now an attendee!"
+      flash[:notice] = "U are now an attendee!"
       redirect_to user_path(current_user)
     else
-      flash.now[:error] = "Rats! Fix your mistakes, please."
-      # no view exists for invitations so :new doesnot exist as well 
-      render :new, status: :unprocessable_entity
+      flash.now[:notice] = "Couldn't attend due to an error"
+      redirect_back fallback_location: '/events'
     end
 
   end
 
   def destroy
-    #params[id] gives the argument in view's @invitation variable isnt used in destroying
-    # because button directs here params[:id] becomes that of the invitation so we simply use that to get the invitation we wanna delete
-    @invitation = Invitation.find(params[:id]) 
+    current_event = Event.find(params[:event_id])
+    @invitation = current_user.invitations.find_by('attended_event_id = ?', current_event.id)
+
     @invitation.destroy
-
-    redirect_to user_path(current_user), status: :see_other
+    redirect_back fallback_location: "/events/#{current_event.id}"
   end
 
-  private
-  
-  def invitations_params
-    # we pass the require information and name it attended_event_id cause that's what it is in the invitations table
-    params.require(:invitation).permit(:attended_event_id)
-  end
 end
