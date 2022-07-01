b2d7b97e5ec465e1c1be757f68da47ac96af3268
shrey-Odinproject <sonishreyf1@gmail.com>
2022-07-01 19:42:07 +0530

    Add #invite method for users that adds str(user.id) for all invited user to invited array, configure route for post user#invite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     @user = current_user # if we dont authenticate then there is no current_user and error happen
     @created_events = @user.created_events
   end
+
+  def invite
+    @user_to_add = params[:user_id]
+    @creator = User.find(params[:creator_id])
+    @creator.invited |= [@user_to_add.to_s] # add only if not already included
+    @creator.save
+    
+    flash[:notice] = "Invite sent!"
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   devise_for :users
   # , :controllers => {registrations: 'registrations'}
   resources :users, only: [:show] # resources user should be below devise user to work
+  resources :users do
+    member do
+      post 'invite'
+    end
+  end
+  
   
   resources :invitations, only:[:new, :create, :destroy]
   
